00446c45fd4ae9a6f1949d97a7705dbc70593851
Léa DUKAEZ <lea.dukaez@gmail.com>
2020-05-01 20:03:01 +0200

    adjustment multi users in Adding Test Result View

--- Insanity/Controller/TestViewController.swift
+++ Insanity/Controller/TestViewController.swift
@@ -12,44 +12,39 @@ import Firebase
 class TestViewController: UIViewController {
     
     let db = Firestore.firestore()
-    var leaWorkoutTest = [String]()
-    var malekWorkoutTest = [String]()
+    var userSelected = K.FStore.users[0]
+    var listWorkoutTest = [String]()
     var textFieldArray = [UITextField]()
     let alert = UIAlertController(title: "Incomplet", message: "Merci de remplir tous les exercices", preferredStyle: UIAlertController.Style.alert)
     let forbiddenNumber = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"]
 
     
+    @IBOutlet weak var usersPickerView: UIPickerView!
     @IBOutlet weak var validateButton: UIButton!
     
-    @IBOutlet weak var leaSKTextField: UITextField!
-    @IBOutlet weak var malekSKTextField: UITextField!
-    @IBOutlet weak var leaPJKTextField: UITextField!
-    @IBOutlet weak var malekPJKTextField: UITextField!
-    @IBOutlet weak var leaPKTextField: UITextField!
-    @IBOutlet weak var malekPKTextField: UITextField!
-    @IBOutlet weak var leaPJTextField: UITextField!
-    @IBOutlet weak var malekPJTextField: UITextField!
-    @IBOutlet weak var leaJSQTextField: UITextField!
-    @IBOutlet weak var malekJSQTextField: UITextField!
-    @IBOutlet weak var leaSJTextField: UITextField!
-    @IBOutlet weak var malekSJTextField: UITextField!
-    @IBOutlet weak var leaPUJKTextField: UITextField!
-    @IBOutlet weak var malekPUJKTextField: UITextField!
-    @IBOutlet weak var leaPMCTextField: UITextField!
-    @IBOutlet weak var malekPMCTextField: UITextField!
+    @IBOutlet weak var SKTextField: UITextField!
+    @IBOutlet weak var PJKTextField: UITextField!
+    @IBOutlet weak var PKTextField: UITextField!
+    @IBOutlet weak var PJTextField: UITextField!
+    @IBOutlet weak var JSQTextField: UITextField!
+    @IBOutlet weak var SJTextField: UITextField!
+    @IBOutlet weak var PUJKTextField: UITextField!
+    @IBOutlet weak var PMCTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textFieldArray = [leaSKTextField, malekSKTextField, leaPJKTextField, malekPJKTextField, leaPKTextField, malekPKTextField, leaPJTextField, malekPJTextField, leaJSQTextField, malekJSQTextField, leaSJTextField, malekSJTextField, leaPUJKTextField, malekPUJKTextField, leaPMCTextField, malekPMCTextField]
+        usersPickerView.dataSource = self
+        usersPickerView.delegate = self
+        
+        textFieldArray = [SKTextField, PJKTextField, PKTextField, PJTextField, JSQTextField, SJTextField, PUJKTextField, PMCTextField]
 
         for textField in textFieldArray {
             textField.delegate = self
             textField.smartInsertDeleteType = UITextSmartInsertDeleteType.no
             textField.keyboardToolbar.doneBarButton.setTarget(self, action: #selector(doneButtonClicked))
         }
-        
     }
     
     @IBAction func validatePressed(_ sender: UIButton) {
@@ -58,36 +53,20 @@ class TestViewController: UIViewController {
         
         if allHaveText {
             for textField in textFieldArray {
-                if textFieldArray.firstIndex(where: {$0 == textField})! % 2 == 0 {
-                    leaWorkoutTest.append(textField.text!)
-                } else {
-                    malekWorkoutTest.append(textField.text!)
-                }
+                listWorkoutTest.append(textField.text!)
             }
-            // Add a new document in Firestore for Lea
+            
+            // Add a new document in Firestore for userSelected
             db.collection(K.FStore.collectionName).addDocument(data: [
-                K.FStore.userField: K.FStore.leaUser,
-                K.FStore.testField: leaWorkoutTest,
+                K.FStore.userField: userSelected,
+                K.FStore.testField: listWorkoutTest,
                 K.FStore.dateField: Timestamp(date: Date())
             ]) { error in
                 if let err = error {
                     print("Error adding document: \(err)")
                 } else {
                     print("Document added!")
-                    self.leaWorkoutTest = [String]()
-                }
-            }
-            // Add a new document in Firestore for Malek
-            db.collection(K.FStore.collectionName).addDocument(data: [
-                K.FStore.userField: K.FStore.malekUser,
-                K.FStore.testField: malekWorkoutTest,
-                K.FStore.dateField: Timestamp(date: Date())
-            ]) { error in
-                if let err = error {
-                    print("Error adding document: \(err)")
-                } else {
-                    print("Document added!")
-                    self.malekWorkoutTest = [String]()
+                    self.listWorkoutTest = [String]()
                 }
             }
             // dismiss view
@@ -112,7 +91,7 @@ class TestViewController: UIViewController {
     
     
     @objc func doneButtonClicked(_ sender: UITextField) {
-        if sender == malekPMCTextField {
+        if sender == PMCTextField {
             sender.resignFirstResponder()
         } else {
             if let senderIndex = textFieldArray.firstIndex(where: {$0 == sender}) {
@@ -126,19 +105,16 @@ class TestViewController: UIViewController {
 extension TestViewController: UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
         let text = (textField.text! as NSString).replacingCharacters(in: range, with: string)
 
         //Prevent "0" characters to be followed by other number
         if forbiddenNumber.contains(text) {
             return false
         }
-
         //Limit the character count to 3.
         if ((textField.text!) + string).count > 3 {
             return false
         }
-
         //Only allow numbers. No Copy-Paste text values.
         let allowedCharacterSet = CharacterSet.init(charactersIn: "0123456789")
         let textCharacterSet = CharacterSet.init(charactersIn: textField.text! + string)
@@ -146,10 +122,50 @@ extension TestViewController: UITextFieldDelegate {
             return false
         }
         return true
-        
+    }
+}
 
+
+extension TestViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return K.FStore.users.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 80
     }
     
 }
 
+extension TestViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
 
+        let myView = UIView(frame: CGRect(x: 0, y: 0, width: pickerView.bounds.width - 30, height: 70))
+
+        let myImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 65, height: 65))
+
+        var rowString = String()
+        rowString = K.userCell.usersLabel[row]
+        myImageView.image = UIImage(named: K.userCell.usersAvatar[row])
+ 
+        let myLabel = UILabel(frame: CGRect(x: 80, y: 0, width: pickerView.bounds.width - 90, height: 70 ))
+        myLabel.text = rowString
+        myLabel.font = .systemFont(ofSize: 22, weight: .semibold)
+        myLabel.textColor = .white
+
+        myView.addSubview(myLabel)
+        myView.addSubview(myImageView)
+
+        return myView
+    }
+    
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        userSelected = K.FStore.users[row]
+    }
+}
